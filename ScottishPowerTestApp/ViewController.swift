16e028dbed0705e1ce46f9ea27fa6e2fa1c63b5b
Amit Vaidya <amit.vaidya@bt.com>
2021-12-02 19:58:11 +0000

    Initial Commit

---
+++ ScottishPowerTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScottishPowerTestApp
+//
+//  Created by Amit Vaidya on 02/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
